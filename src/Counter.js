c797973f99a24b318189410d26e41d69eaaa8807
AleksanderSzczuchniak <alex.szczuchniak@gmail.com>
2018-12-11 15:01:28 +0100

    todoRedux

--- src/Counter.js
+++ src/Counter.js
@@ -4,33 +4,33 @@ import { connect } from 'react-redux'
 import { incAction, decAction } from './state/counter'
 
 const Counter = (props) => (
-  <div>
-    <h1>
-      {props._counterVal}
-    </h1>
-    <button
-      onClick={props._incAction}
-    >
-      +
+    <div>
+        <h1>
+            {props._counterVal}
+        </h1>
+        <button
+            onClick={props._incAction}
+        >
+            +
     </button>
-    <button
-      onClick={props._decAction}
-    >
-      -
+        <button
+            onClick={props._decAction}
+        >
+            -
     </button>
-  </div>
+    </div>
 )
 
 const mapStateToProps = state => ({
-  _counterVal: state.counterReducerName.counter
+    _counterVal: state.counterReducerName.counter
 })
 
 const mapDispatchToProps = dispatch => ({
-  _incAction: () => dispatch(incAction()),
-  _decAction: () => dispatch(decAction())
+    _incAction: () => dispatch(incAction()),
+    _decAction: () => dispatch(decAction())
 })
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(Counter)
--- src/store.js
+++ src/store.js
@@ -1,7 +1,15 @@
 import { createStore, combineReducers } from 'redux'
 import counterReducer, { incAction, decAction } from './state/counter'
+import toDo, {
+    newTaskAction,
+    addNewTaskAction,
+    filterAllTasksAction,
+    filterCompletedTasksAction,
+    filterUncompletedTasksAction
+} from './state/toDo'
 
 const reducer = combineReducers({
+    toDo,
   counterReducerName: counterReducer
 })
 
@@ -12,3 +20,9 @@ export const store = createStore(
 
 window.dispatchIncAction = () => store.dispatch(incAction())
 window.dispatchDecAction = () => store.dispatch(decAction())
+
+window.newTaskAction = text => store.dispatch(newTaskAction(text))
+window.addNewTaskAction = () => store.dispatch(addNewTaskAction())
+window.filterAllTasksAction = () => store.dispatch(filterAllTasksAction())
+window.filterCompletedTasksAction = () => store.dispatch(filterCompletedTasksAction())
+window.filterUncompletedTasksAction = () => store.dispatch(filterUncompletedTasksAction())
